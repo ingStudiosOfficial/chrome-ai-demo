c31823bed963718126eca53ff00a364947ab64a8
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2026-03-19 16:31:30 +0800

    add prompt features

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import ChatView from "@/views/ChatView.vue";
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [{ path: "/", name: "chat", component: ChatView }];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: routes,
+});
 
-export default router
+export default router;
---
+++ src/utils/prompt.ts
@@ -0,0 +1,83 @@
+let session: LanguageModel | null = null;
+
+export async function generateContent(message: string, conversation: LanguageModelMessage[], onDownload: (loaded: number) => void, onMessage: (message: string) => void, onChunk: (chunk: string) => void, images?: Blob[], audios?: Blob[]): Promise<string> {
+	if (!('LanguageModel' in window)) throw new Error('Prompt API not available.');
+
+	if (!session) {
+		onMessage('Checking for Prompt API availability...');
+
+		const availability = await LanguageModel.availability();
+
+		switch (availability) {
+			case 'available':
+				console.log('Prompt API available.');
+				break;
+
+			case 'downloadable':
+				console.log('Prompt API downloadable.');
+				break;
+
+			case 'downloading':
+				console.log('Prompt API downloading.');
+				break;
+
+			case 'unavailable':
+				throw new Error('Prompt API unavailable.');
+		}
+
+		onMessage('Creating LanguageModel instance...');
+
+		const systemPrompt: LanguageModelSystemMessage = { role: 'system', content: [{ type: 'text', value: 'You are Gemini Nano, part of the Chrome Built-in AI. You are friendly and hlpful.' }] };
+
+		session = await LanguageModel.create({
+			monitor(m) {
+				m.addEventListener('downloadprogress', (e) => {
+					onDownload(e.loaded);
+				});
+			},
+			initialPrompts: [systemPrompt, ...conversation],
+			expectedInputs: [{ type: 'text' } /*{ type: 'image' }, { type: 'audio' }*/],
+			expectedOutputs: [{ type: 'text' }],
+		});
+	}
+
+	onMessage('Generating response...');
+
+	const imageAttachments: LanguageModelMessageContent[] = [];
+	const audioAttachments: LanguageModelMessageContent[] = [];
+
+	if (images && images.length !== 0) {
+		for (const image of images) {
+			imageAttachments.push({ type: 'image', value: image });
+		}
+	}
+
+	if (audios && audios.length !== 0) {
+		for (const audio of audios) {
+			audioAttachments.push({ type: 'audio', value: audio });
+		}
+	}
+
+	const stream = session.promptStreaming([
+		{
+			role: 'user',
+			content: [
+				{
+					type: 'text',
+					value: message,
+				},
+				...imageAttachments,
+				...audioAttachments,
+			],
+		},
+	]);
+
+	let response: string = '';
+
+	for await (const chunk of stream) {
+		onChunk(chunk);
+		response += chunk;
+	}
+
+	return response;
+}
